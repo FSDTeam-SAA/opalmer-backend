dc527cc088650a713823aded69b92f4fddad0c1f
zihadulislam2 <zihadul708@gmail.com>
2025-08-06 00:24:54 +0600

    attendance model and interface create

---
+++ src/models/attendance.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose'
+import { IAttendance, AttendanceModel } from '../interface/attendance.interface'
+
+const attendanceSchema: Schema<IAttendance> = new Schema(
+  {
+    classId: { type: Schema.Types.ObjectId, ref: 'Class', required: true },
+    userId: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    present: { type: Boolean, default: true },
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+)
+
+export const Attendance = mongoose.model<IAttendance, AttendanceModel>(
+  'Attendance',
+  attendanceSchema
+)
