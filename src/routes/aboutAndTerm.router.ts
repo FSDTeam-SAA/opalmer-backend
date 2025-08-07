ed3340631cecaeb0c7de4340d8759b5b0754ab7a
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-07 06:20:37 +0600

    about and term crud operation completed

--- src/routes/aboutAndTerm.router.ts
+++ src/routes/aboutAndTerm.router.ts
@@ -8,4 +8,6 @@ router.post("/create", aboutAndTermController.createAboutAndTerm);
 router.get("/about", aboutAndTermController.getAbout);
 router.get("/terms", aboutAndTermController.getTerms);
 
+router.put("/update/:id", aboutAndTermController.updateAboutAndTerm);
+
 export const aboutAndTermRouter = router;
