ad872d15e12e3992103a99316ef00086fc43216e
zihadulislam2 <zihadul708@gmail.com>
2025-08-08 00:09:48 +0600

    Add class routes middlewares

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { featuresAndQuestionsRouter } from './featuresAndQuestions.router'
 import userRoutes from '../routes/user.routes'
 import stuAssignToClassRoutes from '../routes/stuAssignToClass.routes'
+import classRoutes from '../routes/class.routes'
 const router = Router()
 
 const moduleRoutes = [
@@ -13,10 +14,14 @@ const moduleRoutes = [
     path: '/users',
     route: userRoutes,
   },
-  { 
+  {
     path: '/student-assign-to-class',
-    route: stuAssignToClassRoutes
-  }
+    route: stuAssignToClassRoutes,
+  },
+  {
+    path: '/classes',
+    route: classRoutes,
+  },
 ]
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route))
