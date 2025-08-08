422deb9908f95b188797be90040b79500820f44a
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-09 04:01:15 +0600

    school model and interface crated

---
+++ src/interface/school.interface.ts
@@ -0,0 +1,15 @@
+export interface ISchool {
+  name: string;
+  code?: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+  country?: string;
+  phone?: string;
+  email?: string;
+  establishedYear?: number;
+  logo?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
