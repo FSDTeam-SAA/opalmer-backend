287f30aea40cd1db4d0bbcc5e979d3c887c416ed
zihadulislam2 <zihadul708@gmail.com>
2025-08-12 00:49:04 +0600

    setup the socket io

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import app from './app'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
+const httpserver = createServer(app)
+
+export const io = new Server(httpserver, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  httpserver.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
   })
 })
