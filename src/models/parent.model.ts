1afbfbc30b9bbb7e697c80c912dfba39d597edeb
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-15 01:19:41 +0600

    parent created isue fixed

--- src/models/parent.model.ts
+++ src/models/parent.model.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IParent } from "../interface/parent.interface";
+import bcrypt from "bcrypt";
 
 const parentSchema = new Schema<IParent>({
   name: { type: String, required: true },
@@ -25,7 +26,7 @@ const parentSchema = new Schema<IParent>({
     required: true,
   },
   address: { type: String },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String },
   avatar: {
     public_id: { type: String, default: "" },
     url: { type: String, default: "" },
@@ -33,6 +34,24 @@ const parentSchema = new Schema<IParent>({
   schoolId: { type: Schema.Types.ObjectId, ref: "School" },
   childrenId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
+
+parentSchema.pre("save", async function (next) {
+  const user = this as any;
+
+  if (user.isModified("password")) {
+    const saltRounds = Number(process.env.BCRYPT_SALT_ROUND) || 10;
+    user.password = await bcrypt.hash(user.password, saltRounds);
+  }
+
+  next();
+});
+
+parentSchema.statics.isPasswordMatched = async function (
+  plainTextPassword: string,
+  hashPassword: string
+) {
+  return await bcrypt.compare(plainTextPassword, hashPassword);
+};
 
 const Parent = model<IParent>("Parent", parentSchema);
 export default Parent;
