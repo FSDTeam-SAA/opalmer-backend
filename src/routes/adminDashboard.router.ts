d5638ab8c4d1c7b98fd46e81160963fc7df58ada
zihadulislam2 <zihadul708@gmail.com>
2025-08-27 04:53:49 +0600

    feat(stats): getStudentGenderStats

--- src/routes/adminDashboard.router.ts
+++ src/routes/adminDashboard.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
-import { getUserStats } from '../controllers/adminDashboard.controller'
+import {
+  getStudentGenderStats,
+  getUserStats,
+} from '../controllers/adminDashboard.controller'
 import { protect } from '../middlewares/auth.middleware'
 
 const router = Router()
 
 router.get('/stats', protect, getUserStats)
+router.get('/stats/student-gender',
+    //  protect,
+      getStudentGenderStats)
 
 export default router
