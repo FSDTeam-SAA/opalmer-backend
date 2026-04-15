9afc5ae7c37bd28946d522befda4ea968ff4844a
zihadulislam2 <zihadul708@gmail.com>
2026-04-16 00:19:16 +0600

    feat: remove deprecated routes and services
    - Deleted various route files including aboutAndTerm, academicDocument, adminDashboard, attendance, behavior, class, featuresAndQuestions, homeWork, learningTip, lesson, message, notification, parentsChild, quiz, quizQA, quizResult, room, school, stuAssignToClass, and user routes.
    - Removed associated controllers and services for quiz AI, message socket, notification service, WebRTC socket, and utility functions for email, response handling, pagination, and Cloudinary.
    - Updated index route to reflect the removal of the above routes and added notification route.

--- dist/routes/adminDashboard.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminDashboard_controller_1 = require("../controllers/adminDashboard.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.get("/stats", auth_middleware_1.protect, adminDashboard_controller_1.getUserStats);
-router.get("/stats/student-gender", auth_middleware_1.protect, adminDashboard_controller_1.getStudentGenderStats);
-exports.default = router;
