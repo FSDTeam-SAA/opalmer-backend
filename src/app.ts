78b4ecd3bfaa3e83dfa7beb7f1213bbd0d75fd0d
Sajjad177 <sajjadhossainx06@gmail.com>
2026-04-23 02:10:44 +0600

    cor's error api fixed

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import { globalErrorHandler } from './middlewares/globalErrorHandler'
-import { notFound } from './middlewares/notFound'
-import router from './routes'
-const app = express()
+import cors from "cors";
+import express from "express";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
+import router from "./routes";
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.path}`);
   next();
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // app.get('/', (_req, res) => {
 //   res.json({ success: true, message: 'Opalmer API is running' })
 // })
-app.get('/health', (_req, res) => {
-    res.json({ success: true, message: 'Opalmer API is running', uptime: process.uptime() })
-})
+app.get("/health", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Opalmer API is running",
+    uptime: process.uptime(),
+  });
+});
 
-app.use('/api/v1', router)
+app.use("/api/v1", router);
 
-app.use(notFound as never)
-app.use(globalErrorHandler)
+app.use(notFound as never);
+app.use(globalErrorHandler);
 
-export default app
+export default app;
